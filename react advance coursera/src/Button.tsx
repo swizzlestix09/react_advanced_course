3444fc335bc93b9e062736d1a9b96cb8d7fd4d6f
swizzlestix09 <angely.rodriguez@gmail.com>
2024-07-17 22:15:25 -0400

    adding onClick to button

--- react advance coursera/src/Button.tsx
+++ react advance coursera/src/Button.tsx
@@ -3,8 +3,12 @@ import { ReactNode } from "react";
 type Props = {
   children: ReactNode
 }
-const Button = ({ children }: Props) => (
-  <button>{children}</button>
-)
+const Button = ({ children }: Props) => {
+
+  const handleClick = () => console.log('Logging in...')
+  return (
+    <button onClick={handleClick}>{children}</button>
+  )
+}
 
 export default Button;
