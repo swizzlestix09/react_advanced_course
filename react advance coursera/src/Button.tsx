bb7c3cbe95313b0662fe171bcfaa1575c4755c9e
swizzlestix09 <angely.rodriguez@gmail.com>
2024-07-23 11:06:51 -0400

    adding success styles for variant

--- react advance coursera/src/Button.tsx
+++ react advance coursera/src/Button.tsx
@@ -6,10 +6,20 @@ type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
   color: string,
 }
 
+/**
+   * Challenge:
+   *
+   * Accept a `variant` prop and style the Button component
+   * accordingly. The values can be `success`, `warning`, or `danger`.
+   * Check the Figma design for the specific colors to be used for each
+   * variant.
+   */
+
+
 const Button = ({ children, color, size, ...rest }: Props) => {
 
   return (
-    <button className={`${size} ${color}`} {...rest}> {children}</button >
+    <button className={`${size} ${color} ${'success'}`} {...rest}> {children}</button >
   )
 }
 
