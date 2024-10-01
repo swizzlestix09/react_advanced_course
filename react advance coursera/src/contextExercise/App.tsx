586ed77bf8ab75b281000b209d8eae9f4e392876
swizzlestix09 <angely.rodriguez@gmail.com>
2024-10-01 14:29:16 -0400

    adding theme to div in app

--- react advance coursera/src/contextExercise/App.tsx
+++ react advance coursera/src/contextExercise/App.tsx
@@ -1,13 +1,14 @@
 import Header from "./Header"
 import Button from "./Button"
 import './index.css'
-import ThemeProvider from "./ThemeContext"
+import { ThemeContext } from "./ThemeContext"
 import Menu from "../assets/menu/Menu"
 import MenuButton from "../assets/menu/MenuButton"
 import MenuDropdown from "../assets/menu/MenuDropdown"
 import MenuItem from "../assets/menu/MenuItem"
 
 import MenuThemeProvider from "../assets/menu/MenuContext"
+import { useContext } from "react"
 
 
 
@@ -15,24 +16,23 @@ import MenuThemeProvider from "../assets/menu/MenuContext"
 
 export default function App() {
   const sports = ["Tennis", "Pickleball", "Racquetball", "Squash"]
-
+  const { colorTheme } = useContext(ThemeContext)
   return (
-    <ThemeProvider>
-      <MenuThemeProvider>
-        <Menu>
-          <MenuButton>Sports</MenuButton>
-          <MenuDropdown>
-            {sports.map(sport => (
-              <MenuItem key={sport}>{sport}</MenuItem>
-            ))}
-          </MenuDropdown>
-        </Menu>
-        <div className="container dark-theme">
-          <Header />
-          <Button />
-        </div>
-      </MenuThemeProvider>
-    </ThemeProvider>
+    <MenuThemeProvider>
+      <Menu>
+        <MenuButton>Sports</MenuButton>
+        <MenuDropdown>
+          {sports.map(sport => (
+            <MenuItem key={sport}>{sport}</MenuItem>
+          ))}
+        </MenuDropdown>
+      </Menu>
+      <div className={`container ${colorTheme}-theme`}>
+        <Header />
+        <Button />
+      </div>
+    </MenuThemeProvider >
+
   )
 }
 
--- react advance coursera/src/main.tsx
+++ react advance coursera/src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './contextExercise/App'
 //import App from './App'
 import './index.css'
+import ThemeProvider from './contextExercise/ThemeContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
